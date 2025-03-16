726ea0407db87a58e38d6f896627808da5dd0cfd
RedSTARO <822858602@qq.com>
2025-03-16 19:26:48 +0800

    fixup! NowPlayingActivity: Display song metadata in now playing view * Change context used to Application context so not leak connection to service
    Signed-off-by: RedSTARO <822858602@qq.com>

--- app/src/main/java/com/redstar/redefinencm/activity/NowPlayingActivity.kt
+++ app/src/main/java/com/redstar/redefinencm/activity/NowPlayingActivity.kt
@@ -1,22 +1,43 @@
 package com.redstar.redefinencm.activity
 
 import android.content.ComponentName
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import coil.compose.AsyncImage
+import com.redstar.redefinencm.RedefineNCMApplication
 import com.redstar.redefinencm.services.playbackService
 import com.redstar.redefinencm.ui.theme.RedefineNCMTheme
 import kotlinx.coroutines.guava.await
@@ -50,7 +71,7 @@ class NowPlayingActivity : ComponentActivity() {
 @Composable
 fun PlaybackController() {
     var mediaController by remember { mutableStateOf<MediaController?>(null) }
-    val context = LocalContext.current
+    val context = RedefineNCMApplication.getApplicationContext() as Context
 
     LaunchedEffect(Unit) {
         val sessionToken = SessionToken(context, ComponentName(context, playbackService::class.java))
