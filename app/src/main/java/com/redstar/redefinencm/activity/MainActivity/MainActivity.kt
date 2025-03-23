d4c399c0ec4af40cd237b415b03e5480751fdf7d
RedSTARO <822858602@qq.com>
2025-03-23 18:30:49 +0800

    feat: Refactor to Scaffold and NavigationBar for main layout and navigation
    *   **MainActivity:**
        *   Refactor the main layout to use `Scaffold` for a structured UI.
        *   Implement a `NavigationBar` for bottom navigation between "Home" and "My" (user playlists).
        *   Introduce `NavigationItem` data class to manage navigation item properties.
        *   Move `MainScreen` to `MainActivity` to add navigationBar and Scaffold.
        *   Use `NavHost` to handle navigation between "my," "home," and "playlistDetailPage/{songId}".
        *   Add `uid` to `MainActivity` to pass `uid` to `NavHost` and pages.
        *   Move `showUserPlaylistPage` and `showPlaylistDetailPage` to `NavHost`
    *   **NavHost:**
        *   Remove `NavHost.kt`.
    *   **PlaceHolderScreen:**
        *   Add `PlaceHolderScreen` for future content in the "Home" section.
    *   **dependencies:** Add `navigation-compose` and `activity-compose` dependency.
    *   Add placeholder route "home".
    
    Signed-off-by: RedSTARO <822858602@qq.com>

--- app/src/main/java/com/redstar/redefinencm/activity/MainActivity/MainActivity.kt
+++ app/src/main/java/com/redstar/redefinencm/activity/MainActivity/MainActivity.kt
@@ -11,10 +11,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,16 +29,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.MediaMetadata
 import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.google.common.util.concurrent.MoreExecutors
 import com.redstar.redefinencm.BuildConfig
 import com.redstar.redefinencm.RedefineNCMApplication
 import com.redstar.redefinencm.activity.NowPlayingActivity
+import com.redstar.redefinencm.api.NCMApi
+import com.redstar.redefinencm.api.RetrofitInstance
 import com.redstar.redefinencm.services.playbackService
 import com.redstar.redefinencm.ui.theme.RedefineNCMTheme
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -48,8 +62,26 @@ class MainActivity : ComponentActivity() {
                 val context = RedefineNCMApplication.getApplicationContext() as Context
                 val metadataFlow = remember { MutableStateFlow<MediaMetadata?>(null) }
                 val metadata by metadataFlow.collectAsState()
+                val navController = rememberNavController()
+                if (BuildConfig.DEBUG) {
+                    Log.d("Main", "MainStartup, Nav Controller: $navController")
+                }
+                val retrofit = RetrofitInstance.retrofit.create(NCMApi::class.java)
+                var uid by remember { mutableStateOf<Long?>(null) }
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                val items = listOf(
+                    NavigationItem("Home", Icons.Filled.Home, "home"),
+                    NavigationItem("My", Icons.Filled.Person, "my"),
+//                    NavigationItem("Settings", Icons.Filled.Settings, "settings")
+                )
 
                 LaunchedEffect(Unit) {
+                    uid = retrofit.userAccount().account.id
+                    if (BuildConfig.DEBUG) {
+                        Log.d("Main", "UID: $uid")
+                    }
                     val sessionToken =
                         SessionToken(context, ComponentName(context, playbackService::class.java))
                     val controllerFuture =
@@ -81,9 +113,56 @@ class MainActivity : ComponentActivity() {
                             AsyncImage(model = metadata?.artworkUri, contentDescription = null)
                         }
                     },
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEach { item ->
+                                NavigationBarItem(
+                                    icon = { Icon(item.icon, contentDescription = item.label) },
+                                    label = { Text(item.label) },
+                                    selected = currentRoute == item.route,
+                                    onClick = {
+                                        navController.navigate(item.route) {
+                                            // To prevent back navigation
+                                            popUpTo(navController.graph.startDestinationId)
+                                            launchSingleTop = true
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    },
                 ) { innerPadding ->
                     Surface {
-                        MainScreen()
+                        if (uid != null) {
+                            // 使用 NavHost 管理导航
+                            NavHost(
+                                navController = navController,  // 传入 NavController 实例
+                                startDestination = "my"       // 指定开始的页面
+                            ) {
+                                composable("my") {
+                                    showUserPlaylistPage(
+                                        retrofit = retrofit,
+                                        uid = uid!!,
+                                        navController = navController
+                                    )
+                                }
+                                composable("home") {
+                                    PlaceHolderScreen()
+                                }
+
+                                composable("playlistDetailPage/{songId}") { backStackEntry ->
+                                    val songId = backStackEntry.arguments?.getString("songId")
+                                    if (BuildConfig.DEBUG) {
+                                        Log.d("Main", "SongList ID: $songId")
+                                    }
+                                    showPlaylistDetailPage(
+                                        retrofit = retrofit,
+                                        songlistID = songId!!.toLong(),
+                                        navController = navController
+                                    )
+                                }
+                            }
+                        }
                         Spacer(modifier = Modifier.padding(innerPadding))
                     }
                 }
@@ -100,7 +179,6 @@ class MainActivity : ComponentActivity() {
         val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
         controllerFuture.addListener({
             // MediaController is available here with controllerFuture.get()
-
         }, MoreExecutors.directExecutor())
 
     }
@@ -113,4 +191,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+data class NavigationItem(
+    val label: String,
+    val icon: ImageVector,
+    val route: String
+)
---
+++ app/src/main/java/com/redstar/redefinencm/activity/MainActivity/PlaceHolderScreen.kt
@@ -0,0 +1,11 @@
+package com.redstar.redefinencm.activity.MainActivity
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PlaceHolderScreen() {
+    Text("Under construction", Modifier.fillMaxWidth())
+}
