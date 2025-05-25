f5e5b1308c139261b55fb9381c3c96dd9b6255c0
RedSTARO <822858602@qq.com>
2025-05-25 14:29:12 +0800

    Revert "fixup! feat: Defer song URL fetching until playback"
    This reverts commit 2ac17cb872ddf22d93dd170745c06e3f7f2fcd07.

--- app/src/main/java/com/redstar/redefinencm/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/redstar/redefinencm/viewmodel/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.redstar.redefinencm.viewmodel
 
 import android.content.ComponentName
-import android.net.Uri
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -185,7 +184,6 @@ class MainViewModel(
         }
         viewModelScope.launch {
             val mediaItem = MediaItem.Builder()
-                .setUri("placeholder://pending/${song.id}".toUri())
                 .setMediaMetadata(
                     MediaMetadata.Builder()
                         .setTitle(song.name)
@@ -197,9 +195,6 @@ class MainViewModel(
                 )
                 .setMediaId(song.id.toString())
                 .build()
-            Log.d("showPlaylistDetail", mediaItem.mediaId)
-            Log.d("showPlaylistDetail", mediaItem.localConfiguration?.uri.toString())
-            Log.d("showPlaylistDetail", mediaItem.mediaMetadata.title.toString())
             withContext(Dispatchers.Main) {
                 this@MainViewModel.mediaController.value?.clearMediaItems()
                 this@MainViewModel.mediaController.value?.addMediaItem(mediaItem)
@@ -224,7 +219,6 @@ class MainViewModel(
                     targetSongId = index
                 }
                 val mediaItem = MediaItem.Builder()
-                    .setUri("placeholder://pending/${eachSong.id}".toUri())
                     .setMediaMetadata(
                         MediaMetadata.Builder()
                             .setTitle(eachSong.name)
