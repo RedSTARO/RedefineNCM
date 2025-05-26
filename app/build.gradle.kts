20d717086e8b24235ef099406ae73c1cf4419a81
RedSTARO <822858602@qq.com>
2025-05-26 10:56:56 +0800

    build: Increment app version to v0.0.4
    This commit updates the `versionName` in `app/build.gradle.kts` from "v0.0.3" to "v0.0.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = generateVersionCode()
-        versionName = "v0.0.3"
+        versionName = "v0.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionNameSuffix = "_EarlyAccess_${getGitSha().substring(0..5)}"
         buildConfigField("String", "GIT_SHA", "\"${getGitSha()}\"")
