b059064b96cc008b87ede43097283867e824cd73
RedSTARO <822858602@qq.com>
2025-05-28 19:10:45 +0800

    feat: Display download status in playlist detail by icon
    *   **ShowPlaylistDetailPage:**
        *   Show a checkmark icon next to songs that have already been downloaded.
        *   Remove the "Downloaded: true/false" text.

--- app/src/main/java/com/redstar/redefinencm/activity/mainActivity/ShowPlaylistDetailPage.kt
+++ app/src/main/java/com/redstar/redefinencm/activity/mainActivity/ShowPlaylistDetailPage.kt
@@ -14,9 +14,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -212,8 +216,12 @@ fun ShowPlaylistDetailPage(
                             )
                         }
 
-                        Text(text = "Downloaded: " + DownloadUtil.fileAlreadyExistsByBaseName(song.id.toString()).toString())
-
+                        if (DownloadUtil.fileAlreadyExistsByBaseName(song.id.toString())) {
+                            Icon(
+                                imageVector = Icons.Filled.Check,
+                                contentDescription = "Downloaded"
+                            )
+                        }
                     }
                 }
             }
